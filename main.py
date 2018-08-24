c5389b7d3064b8b26147e27b571881b9aa2437e7
hoda5 <josefernandotolentino@gmail.com>
2018-08-24 08:27:52 -0300

    deploy

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import webapp2
 import json
 from google.appengine.api import urlfetch
+
+# https://circleredirect.appspot.com
 
 # http://localhost:7001/$USERNAME/$PROJECT/$BRANCH/$LASTBUILD?u=hoda5&p=workspace&b=master&a=0&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
 # http://localhost:7001/tmp/artefatos/coverage/index.html?u=hoda5&p=workspace&b=master&a=1&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
