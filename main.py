d948925fec8a6c3f5fbee9ddea7b413253b8f687
hoda5 <josefernandotolentino@gmail.com>
2018-08-24 08:55:16 -0300

    proxify

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 
 import webapp2
 import json
+import logging
 from google.appengine.api import urlfetch
 
 # https://circleredirect.appspot.com
 
+
+# send_api
+# http://localhost:7001/api/v1.1/project/github/hoda5/workspace/tree/master?u=hoda5&p=workspace&b=master&a=0&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
 # http://localhost:7001/$USERNAME/$PROJECT/$BRANCH/$LASTBUILD?u=hoda5&p=workspace&b=master&a=0&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
+
+# redirect
 # http://localhost:7001/tmp/artefatos/coverage/index.html?u=hoda5&p=workspace&b=master&a=1&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
-# http://localhost:7001/tmp/artefatos/coverage/index.html?u=hoda5&p=workspace&b=master&a=1&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
+# proxify
+# http://localhost:7001/tmp/artefatos/coverage/index.html?u=hoda5&p=workspace&b=master&a=2&t=874a579716b1ea0f6b6bace02bf96cf9bad55478
+
 # https://circleci.com/api/v1.1/project/github/hoda5/workspace/tree/master?circle-token=874a579716b1ea0f6b6bace02bf96cf9bad55478
 # /project/:vcs-type/:username/:project/:build_num/artifacts
 
@@ -22,7 +30,8 @@ class MainPage(webapp2.RequestHandler):
             self.project = self.request.GET['p']
             self.branch = self.request.GET['b']
             self.token = self.request.GET['t']
-            self.is_artifact = self.request.GET['a'] == "1"
+            self.redir_artifact = self.request.GET['a'] == "1"
+            self.proxy_artifact = self.request.GET['a'] == "2"
 
             self.path = self.path.replace("$USERNAME", self.user_name)        
             self.path = self.path.replace("$PROJECT", self.project)
@@ -30,11 +39,13 @@ class MainPage(webapp2.RequestHandler):
             self.path = self.path.replace("$TOKEN", self.token)
 
             self.parse_build_num()
-            self.parse_artifact()
-            if self.is_artifact:
-              self.send_artficat()
-            else:
-              self.send_api()
+            if self.redir_artifact:
+              return self.redirect_to_artifact()
+
+            if self.proxy_artifact:
+              return self.proxify_artifact()
+            
+            self.send_api()
 
             # self.response.write(self.artifact_root)
            
@@ -83,8 +94,6 @@ class MainPage(webapp2.RequestHandler):
             self.error = result.status_code
 
     def parse_artifact(self):
-        if not self.is_artifact:
-           return
         url = 'https://circleci.com/api/v1.1/project/github/' + self.user_name + "/" + self.project + "/" + self.last_build_num + "/artifacts?circle-token=" + self.token
         headers = {'Accept': 'text/plain'}
         result = urlfetch.fetch(
@@ -93,13 +102,13 @@ class MainPage(webapp2.RequestHandler):
         if result.status_code == 200:
             self.artifacts = json.loads(result.content)
             artifact1 = self.artifacts[0]
-            self.artifact_root = artifact1.get("url").replace(artifact1.get("path"), "")
+            self.artifact_root = artifact1.get("url").replace("/"+artifact1.get("path"), "")
 
         else:
             self.error = result.status_code
 
     def send_api(self):
-        url = 'https://circleci.com/' + self.path
+        url = 'https://circleci.com/' + self.path + "?circle-token=" + self.token
         headers = {'Accept': 'text/plain'}
         result = urlfetch.fetch(
                 url=url,
@@ -107,11 +116,23 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers = result.headers
         self.response.write(result.content)
 
-    def send_artficat(self):
+    def redirect_to_artifact(self):
+        self.parse_artifact()
         url = self.artifact_root + self.path
-        url = url.replace("//", "/")
         self.response.headers['Location'] = str(url)
         self.response.status_int = 307
+
+    def proxify_artifact(self):
+        self.parse_artifact()
+        url = self.artifact_root + self.path + "?circle-token=" + self.token
+        headers = {'Accept': '*/*'}
+        result = urlfetch.fetch(
+                url=url,
+                validate_certificate=True,
+                headers=headers)
+        # self.response.write(url)        
+        self.response.headers = result.headers
+        self.response.write(result.content)
 
 app = webapp2.WSGIApplication([
     (r'.*', MainPage),
